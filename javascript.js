e5a143e281a10ed8885ffb0ba97612d0176b2989
Dima Shubitidze <einherjar73@users.noreply.github.com>
2026-03-30 16:06:55 +0400

    buttons functionality given halfly added some html css

--- javascript.js
+++ javascript.js
@@ -2,17 +2,29 @@ const adding = document.querySelector(".adding");
 const dialogWindow = document.querySelector(".window"); 
 const addBook = document.querySelector(".addBook");
 const closing = document.querySelector(".close");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
 
 adding.addEventListener("click", () => {
   dialogWindow.style.visibility = "visible";
 });
 
-closing.addEventListener("click", () => {
-  dialogWindow.style.visibility = "hidden";
+closing.addEventListener("click", () => {  
+  clearFields();
+  hideWindow();
 })
 
 addBook.addEventListener("click", () => {
-  
+  let savedTitle = title.value;
+  let savedAuthor = author.value;
+  let savedPages = pages.value;
+  let savedRead = read.checked;
+
+  addBookToLibrary(savedTitle, savedAuthor, savedPages, savedRead);
+  clearFields();
+  hideWindow();
 });
 
 const myLibrary = [];
@@ -26,12 +38,23 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  let book1 = new Book(title, author, pages, read);
-  myLibrary.push(book1);
+  let book = new Book(title, author, pages, read);
+  myLibrary.push(book);
 }
 
 function showOnDisplay() {
   for(let i = 0; i < myLibrary.length; i++){
     
   };
+}
+
+function clearFields() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.value = "false";
+}
+
+function hideWindow() {
+  dialogWindow.style.visibility = "hidden";
 }
